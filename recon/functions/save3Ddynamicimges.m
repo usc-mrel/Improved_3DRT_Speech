a5dd1817be9070d2027e49ee04bd8d74988fd1ee
Ziwei Zhao <52764357+ziwei626@users.noreply.github.com>
2020-10-05 16:13:58 -0700

    Update save3Ddynamicimges.m

--- recon/functions/save3Ddynamicimges.m
+++ recon/functions/save3Ddynamicimges.m
@@ -15,5 +15,5 @@ function result = save3Ddynamicimges (img, rot_angle, num_col, num_row)
 nframes = size(img,4);
 result = zeros(size(img,1)*num_row, size(img,2)*num_col,nframes);
 for t=1:nframes
-    result(:,:,t) =f_save_3d_static_img(img(:,:,:,t), rot_angle, num_col, num_row);
+    result(:,:,t) =save3Dstaticimges(img(:,:,:,t), rot_angle, num_col, num_row);
 end
