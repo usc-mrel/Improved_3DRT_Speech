a24eaf9ffb4bb8686b2e7ea791af48cea3e429a1
Ziwei Zhao <52764357+ziwei626@users.noreply.github.com>
2020-10-05 15:36:21 -0700

    Update f_save_3d_static_img.m

--- recon_code/functions/f_save_3d_static_img.m
+++ recon_code/functions/f_save_3d_static_img.m
@@ -1,5 +1,15 @@
 function result = f_save_3d_static_img (img, rot_angle, num_col, num_row)
 
+% Converts the 3D static images (x-y-z) to concatenated display of 2D image series
+% with specified rotated angle, columns and rows
+%
+% Inputs:   
+%         img:  3D images with x-y-z dimensions
+%   rot_angle:  rotated angle of the images
+%     num_col:  number of columns in the concatenated display of z dimension
+%     num_row:  number of rows in the concatenated display of z dimension
+% Output:
+%      result:  2D concatenated images 
 
 
 result = [];
