30edea7eb251e8fc4455e550e1cca1d0c6e7a2c1
Ziwei Zhao <52764357+ziwei626@users.noreply.github.com>
2020-10-12 16:44:39 -0700

    updated code

--- recon/functions/recon_sos_3d.m
+++ recon/functions/recon_sos_3d.m
@@ -14,31 +14,31 @@
 %  
 %                            06/07/2020
 
-function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
+function [Recon, coilmap, reconInfo] = recon_sos_3d(matfname, param)
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialization
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
+
     imsize        = param.imsize;       % image size [nx ny nz]
     windowsize    = param.windowsize;   % # of time frames for one recon
     slices2recon  = param.slices2recon; % slices to reconstruct 
     coil2recon    = param.coil2recon;   % coil elementes to use
     cfreq         = param.cfreq;        % freq on which to demodulate k-space data based   
-    
+
     lambda_sTV    = param.lambda_sTV;
     lambda_tTV    = param.lambda_tTV;   
     niter         = param.niter; 
     admm_rho      = param.admm_rho;
     folder_index  = param.folder_index;  
     temp_win      = param.tempwin;
-    
+
     TR            = param.TR;
     TE            = 0.45/1000;           
     Ts            = 4e-6;
-    
+
     HanningSize   = [31 31 5];
     displayFigure = false;
-    
+
     % Set up BART library
     cur_dir = pwd;
     base_dir = ['/Users/' getenv('USER')];
@@ -53,13 +53,13 @@ function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
 % Coil calibration w/ fully sampled time-averaged data 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     fprintf('Coil Map Estimation\n');
-    
+
     T0 = tic;
-    
+
     nsprials = size(kdata,2) - 1;
     nc       = size(kdata,3);    % # of coil elements
     ns       = size(kdata,1);    % # of data samples per spiral
-    
+
     kdata3DT = zeros([1, size(w,1), nsprials, nc, 1, 1]);
     kloc3DT  = zeros([3, size(w,1), nsprials, 1,  1, 1]);
     w3DT     = zeros([1, size(w,1), nsprials, 1,  1, 1]);
@@ -70,11 +70,11 @@ function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
         w3DT(1,:,:,1,1,n_frame) = w(:, (n_frame-1)*nsprials+1:n_frame*nsprials);    
         kdata3DT(1,:,:,:,1,n_frame) = kdata(:, (n_frame-1)*nsprials+1:n_frame*nsprials, :);
     end
-    
+
     kloc3DT(1,:) = kloc3DT(1,:)*imsize(1);
     kloc3DT(2,:) = kloc3DT(2,:)*imsize(2);
     kloc3DT(3,:) = kloc3DT(3,:)*imsize(3);
-    
+
     % time-averaged 3D image
     ta3dimg = bart(sprintf('nufft -a -d %d:%d:%d -t',imsize(1),imsize(2),imsize(3)),kloc3DT,kdata3DT.*repmat(w3DT, [1 1 1 nc]));
 
@@ -83,19 +83,19 @@ function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
         sos_ta3dimg_display = f_save_3d_dynamic_img(double(sos_ta3dimg), 90, 4, 3);
         figure(100), imagesc(abs(sos_ta3dimg_display)), axis image off; colormap('gray');
     end
-    
+
     window = generatewindows(HanningSize, 3); % 3D Hanning filter
-    
+
     % time-averaged low-res 3D image
     talr3dimg = convn(ta3dimg, window, 'same'); % Smoothed by convolution with a 3D filter
-    
+
     talr3dksp = bart('fft -u 7', talr3dimg);    % 3DFT to get kspace
-    
+
     talr3dksp_zpz ...                           % zeropad along z-direction
         = bart(sprintf('resize -c 0 %d 1 %d 2 %d', imsize(1), imsize(2), imsize(3)*2), talr3dksp);
     coilmap = bart('ecalib -S -m1', talr3dksp_zpz);
     coilmap = (coilmap(:,:,1:2:end,:) +coilmap(:,:,2:2:end,:))/2;
-    
+
     if displayFigure
         coilmap_display = save3Ddynamicimages(coilmap, 90, 8, 3);
         for cc = 1:nc
@@ -104,27 +104,27 @@ function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
             title(sprintf('Coil sensitivity: %d',cc)); caxis([0 max(abs(coilmap_display(:)))*0.8]);
         end
     end
-    
+
     T = toc(T0);
     fprintf('Coil Map Estimation Done: %f s\n', T);
-    
+
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Data Preparation for Reconstruction
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     fprintf('Data Preparation for Reconstruction\n');    
     T0 = tic;
-    
+
     % Find the first kz index at the bottom
-    if folder_index == 1       % original method
+    if folder_index == 1        % original method
         first_bottom_z_idx = find(kloc(1,:,3) == -0.5);
-        first_bottom_z_idx = first_bottom_z_idx(1);
+        first_bottom_z_idx = first_bottom_z_idx(1) - 1;
     else 
         first_bottom_z_idx = 0; % proposed method
     end
-    
+
     max_nframes = floor((size(kdata,2)-first_bottom_z_idx)/temp_win);    
     nframes = windowsize; 
-    
+
     if mod(max_nframes,nframes) ~= 0
         cat_num = floor(max_nframes/nframes) + 1; 
     else 
@@ -137,19 +137,19 @@ function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
     w3DT     = zeros([1, ns, temp_win, 1,  1, max_nframes]);
 
     for n_frame = 1 : max_nframes    
-        
-        t_idx = (n_frame-1) * temp_win+1 : n_frame * temp_win;
-             
+
+        t_idx = (n_frame-1) * temp_win + 1 : n_frame * temp_win;
+
         if folder_index == 1 
-            t_idx = t_idx + first_bottom_z_idx - 1; 
+            t_idx = t_idx + first_bottom_z_idx; 
         end
-        
+
         kloc_temp  = kloc(:, t_idx, :);
         kloc3DT(:,:,:,1,1,n_frame) = permute( kloc_temp,[3 1 2]);
         w3DT(1,:,:,1,1,n_frame) = w(:, t_idx);    
         kdata3DT(1,:,:,:,1,n_frame) = kdata(:, t_idx, :);
     end
-    
+
     kloc3DT(1,:) = kloc3DT(1,:)*imsize(1);
     kloc3DT(2,:) = kloc3DT(2,:)*imsize(2);
     kloc3DT(3,:) = kloc3DT(3,:)*imsize(3);
@@ -163,108 +163,107 @@ function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
     kdata3DT(1,:,:,:,1,:) = temp;
     kdata2DTslices = bart('fft -i -u 4',kdata3DT); % inverse FT along kz 
 
-    %%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     Recon = zeros(imsize(1),imsize(2),imsize(3),max_nframes);
-    
+
     for ij = 1 : cat_num
         if ij == 1
-           ttt = 1 : nframes + 1; 
+           ttt = 1 : nframes + 2; 
            ddd = 1 : nframes; 
         end
         if ij > 1 && ij < cat_num 
-           ttt = (ij-1) * nframes : ij * nframes + 1;
+           ttt = (ij-1) * nframes -1 : ij * nframes + 2;
            ddd = (ij-1) * nframes +1 : ij * nframes;
         end
         if ij == cat_num
             if mod(max_nframes,nframes) ~= 0
-               ttt = (max_nframes - nframes_end) : max_nframes;
+               ttt = (max_nframes - nframes_end) - 1 : max_nframes;
                ddd = (max_nframes - nframes_end) + 1 : max_nframes;
             else               
-               ttt = (ij-1) * nframes : ij * nframes;
+               ttt = (ij-1) * nframes -1 : ij * nframes;
                ddd = (ij-1) * nframes +1 : ij * nframes;
             end
         end
-        
         if cat_num == 1
              ttt = 1 : nframes; 
              ddd = 1 : nframes; 
         end
-            
-    kdata3DT_input  = kdata3DT(:,:,:,coil2recon,:,ttt);
-    kloc3DT_input   = kloc3DT(:,:,:,:,:,ttt);  
-    w3DT_input      = w3DT(:,:,:,:,:,ttt);
-    coilmap_input   = coilmap(:,:,:,coil2recon);
 
-    weight_name = ['./weight']; 
-    writecfl(weight_name, (w3DT_input));
-    T0 = tic;
-    T = toc(T0);
-    fprintf('Data Preparation Done: %f s\n', T);
+        kdata3DT_input  = kdata3DT(:,:,:,coil2recon,:,ttt);
+        kloc3DT_input   = kloc3DT(:,:,:,:,:,ttt);  
+        w3DT_input      = w3DT(:,:,:,:,:,ttt);
+        coilmap_input   = coilmap(:,:,:,coil2recon);
+
+        weight_name = ['./weight']; 
+        writecfl(weight_name, (w3DT_input));
+        T0 = tic;
+        T = toc(T0);
+        fprintf('Data Preparation Done: %f s\n', T);
 
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 3D Reconstruction
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    fprintf('3D Reconstruction\n');
-    fprintf('[nx ny nz nt] = [%d %d %d %d]\n', imsize(1), imsize(2), imsize(3), nframes);
-    bart_cmd = sprintf('pics -S -d4 -u%f -RT:7:0:%f -R T:32:0:%f -i%d -p %s -t', admm_rho, lambda_sTV, lambda_tTV, niter, weight_name); 
-    disp(bart_cmd);
-           
-    T0 = tic; 
 
-    temp  = bart(bart_cmd, kloc3DT_input, kdata3DT_input, coilmap_input);
-    recon = squeeze(temp);
-    
-    for zz=1:size(recon,3)
-        for tt=1:size(recon,4)
-            recon(:,:,zz,tt) = imrotate(recon(:,:,zz,tt),90); 
-        end
-    end
+        fprintf('3D Reconstruction\n');
+        fprintf('[nx ny nz nt] = [%d %d %d %d]\n', imsize(1), imsize(2), imsize(3), nframes);
+        bart_cmd = sprintf('pics -S -d4 -u%f -RT:7:0:%f -R T:32:0:%f -i%d -p %s -t', admm_rho, lambda_sTV, lambda_tTV, niter, weight_name); 
+        disp(bart_cmd);
 
-    if displayFigure      
-        figure(110),
-        for tt=1:size(recon,4)
-            imagesc(abs(recon(:,:,7,tt))); colormap gray; axis image off;    
-            title(tt); drawnow;
+        T0 = tic; 
+
+        temp  = bart(bart_cmd, kloc3DT_input, kdata3DT_input, coilmap_input);
+        recon = squeeze(temp);
+
+        for zz=1:size(recon,3)
+            for tt=1:size(recon,4)
+                recon(:,:,zz,tt) = imrotate(recon(:,:,zz,tt),90); 
+            end
         end
-    end
-   
-    if cat_num > 1
-     if ij == 1
-          recon = recon(:, :, :, 1 : end-1);   
+
+        if displayFigure      
+            figure(110),
+            for tt=1:size(recon,4)
+                imagesc(abs(recon(:,:,7,tt))); colormap gray; axis image off;    
+                title(tt); drawnow;
+            end
         end
-        if ij > 1 && ij < cat_num
-           recon = recon(:, :, :, 2 : end-1);
+
+        if cat_num > 1
+            if ij == 1
+              recon = recon(:, :, :, 1 : end-2);   
+            end
+            if ij > 1 && ij < cat_num
+               recon = recon(:, :, :, 3 : end-2);
+            end
+            if ij == cat_num
+               recon = recon(:, :, :, 3 : end);
+            end
         end
-        if ij == cat_num
-           recon = recon(:, :, :, 2 : end);
-        end
-    end
-        
-    Recon(:,:,:,ddd) = recon; 
-    
- 
-    T = toc(T0);
-    fprintf('Reconstruction Done: %f s\n', T);
+
+        Recon(:,:,:,ddd) = recon; 
+
+
+        T = toc(T0);
+        fprintf('Reconstruction Done: %f s\n', T);
     end
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Generate reconstruction header    
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % mkdir('./mrm_result');
+
     [pathstr, name, ext]      = fileparts(matfname);
-    
+
     reconInfo                 = struct('Date', date, 'Fname', name, 'recon_method','BART (v4.03) pics', 'recon_time',T);
 
     reconInfo.cmap_method     = 'Espirit';
     reconInfo.imsize          = imsize;       % image size [nx ny nz]
     reconInfo.nframes         = max_nframes;
-    
+
     reconInfo.slices2recon    = slices2recon; % slices to reconstruct 
     reconInfo.coil2recon      = coil2recon;   % coil elementes to use
-    
+
     reconInfo.bart_cmd        = bart_cmd;
     reconInfo.HanningSize     = HanningSize; 
     reconInfo.cfreq           = cfreq;        % freq on which to demodulate k-space data based   
@@ -274,10 +273,11 @@ function [Recon, coilmap, reconInfo] = recon3dsos_3d(matfname, param)
     reconInfo.niter           = niter; 
     reconInfo.admm_rho        = admm_rho;
     reconInfo.windowsize      = windowsize;
-    
+
     reconInfo.TR              = TR;
     reconInfo.TE              = TE;
     reconInfo.Ts              = Ts;
-    
+
     disp('Reconstruction is done now');
+
 end
--- recon/main_recon_3d.m
+++ recon/main_recon_3d.m
@@ -16,8 +16,8 @@ clear all;
 close all; 
 clc;
 
-addpath('../Improved_3DRT_Speech/');    % please change the path
-directory = '../Improved_3DRT_Speech/'; % please change the path
+addpath('../Improved_3DRT_Speech/recon/functions');    % please change the path
+directory = '../Improved_3DRT_Speech/';                % please change the path
 outdirectory = 'results/';
 fileInfo = dir(strcat(directory, 'data/3d_*'));
 index = {'ori','vd'};
@@ -30,35 +30,37 @@ for i  =  1 : length(fileInfo)
     param.imsize       = [84 84 12];  
     param.narms        = 1;     % number of arms per kx-ky plane to use
     param.slices2recon = 1:12;  % slices to reconstruct (1st slice has low SNR so it'd be better not to recon)
-    param.coil2recon   = 7;     % coil dimensions
+    param.coil2recon   = 1:8;   % coil dimensions 
     param.cfreq        = 0;     % center frequency 
     param.lambda_sTV   = 0.008; % parameter for spatial TV
     param.lambda_tTV   = 0.03;  % parameter for temporal TV
-    param.niter        = 5;     % number of iteration in BART recon
+    param.niter        = 300;   % number of iteration in BART recon
     param.admm_rho     = 0.05;  % ADMM parameter
     param.TR           = 5.048/1000; % [ms]
     param.folder_index = i;
     param.tempwin      = 12;    % number of spiral arms per time point
-    param.windowsize   = 20;    % number of time frames for one reconstruction
+    param.windowsize   = 100;   % number of time frames for one reconstruction
      
     %% Reconstruction 
     matfname = fullfile(file.folder, file.name);                     
 
-   [recon, coilmap, reconInfo] = recon_sos_3d(matfname, param);
+    [recon, coilmap, reconInfo] = recon_sos_3d(matfname, param);
 
     %% Save results
     suffix = sprintf('3d_%s_Nt%d_TRs%d_rt%.5f_rs%.5f_frames%d_niter%d', ...
     index{i}, reconInfo.nframes, reconInfo.temp_win, reconInfo.lambda_tTV, reconInfo.lambda_sTV, reconInfo.windowsize, reconInfo.niter);
      
     [pathstr, name, ext] = fileparts(matfname);
-    outpath  = strcat(directory, outdirectory, fileInfo(i).name, '/'); cd(outpath); 
+    
+    outpath  = strcat(directory, outdirectory, fileInfo(i).name, '/'); 
+    mkdir(outpath); cd(outpath); 
     volfname = strcat(name, '_', suffix);
     volfname = strcat(outpath, volfname);
 
     save(strcat(volfname,'.mat'), 'recon', 'coilmap', 'reconInfo');
 
     %% Generate parallel sagittal views video
-    f_disp4D(strcat(volfname,'.avi'), recon(:,:,1:12,:), 1/(reconInfo.temp_win*reconInfo.TR), 0, 4, 3);
+    display4Dimages(strcat(volfname,'.avi'), recon(:,:,1:12,:), 1/(reconInfo.temp_win*reconInfo.TR), 0, 4, 3);
     cd(directory);
     
 end
